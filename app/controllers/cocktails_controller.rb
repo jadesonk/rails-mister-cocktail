782ebeafe30422ba688babdeca9b55a932093675
jadeson komolvipat <jadesonk@gmail.com>
2020-09-10 15:26:01 +0700

    create cocktail index, new, create, and show routes

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,33 @@
+class CocktailsController < ApplicationController
+  before_action :find_cocktail, only: [:show]
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+
+    if @cocktail.save
+      redirect_to @cocktail
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+end
